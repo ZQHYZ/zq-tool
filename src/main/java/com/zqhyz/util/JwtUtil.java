2639da141e29778735fc9a71728ed9f63aad8882
zqhyz <1027774997@qq.com>
2024-03-23 21:17:08 +0800

    增加解析令牌

--- src/main/java/com/zqhyz/util/JwtUtil.java
+++ src/main/java/com/zqhyz/util/JwtUtil.java
@@ -2,16 +2,22 @@ package com.zqhyz.util;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.SignatureGenerationException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.interfaces.Claim;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.Date;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @Slf4j
@@ -39,5 +45,43 @@ public class JwtUtil {
                 .sign(Algorithm.HMAC256(key));
         template.opsForValue().set(Const.JWT_ACCOUNT + username, jwt, expire * 60 * 60L, TimeUnit.SECONDS);
         return jwt;
+    }
+
+    /**
+     * 解析令牌
+     * @param headerToken
+     * @return
+     */
+    public DecodedJWT resolveJwt(String headerToken) {
+        String token = ObjectUtils.isEmpty(headerToken) ? headerToken.substring(7) : null;
+        if (token == null) {
+            // TODO 抛出自定义异常
+            return null;
+        }
+
+        Algorithm algorithm = Algorithm.HMAC256(key);
+        JWTVerifier jwtVerifier = JWT.require(algorithm).build();
+        try {
+//            return jwtVerifier.verify(token);
+            DecodedJWT verify = jwtVerifier.verify(token);
+//            if (this.isInvalidToken(verify.getId())) {
+//                // TODO 抛出自定义异常
+//                return null;
+//            }
+            Map<String, Claim> claims = verify.getClaims();
+//            return new Date().after(claims.get("exp").asDate()) ? null : verify;
+            String usernameOrTel = claims.get("name").asString();
+            String redisJwt = template.opsForValue().get(Const.JWT_ACCOUNT + usernameOrTel);
+            if (ObjectUtils.isEmpty(redisJwt) || (!token.equals(redisJwt))) {
+                return null;
+            }
+            Long expireTime = template.getExpire(Const.JWT_ACCOUNT + usernameOrTel);
+            if (expireTime != null && expireTime < 30 * 60) {
+                template.opsForValue().set(Const.JWT_ACCOUNT + usernameOrTel, token, expire * 60 * 60L, TimeUnit.SECONDS);
+            }
+            return verify;
+        } catch (JWTVerificationException e) {
+            return null;
+        }
     }
 }
